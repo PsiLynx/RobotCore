81c9d6ef27b044b657300899dc5e5ac08bde8d9f
AS1624 <thebest1dudes@gmail.com>
2024-04-16 07:51:51 -0800

    add in line closest point solver

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GVF/Line.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GVF/Line.kt
@@ -4,6 +4,8 @@ import org.firstinspires.ftc.teamcode.util.Vector2D
 import org.firstinspires.ftc.teamcode.util.Vector2D.Companion.mag
 import kotlin.math.atan2
 import kotlin.math.sqrt
+import kotlin.math.sin
+import kotlin.math.PI
 
 class Line(var p1: Vector2D, var p2: Vector2D): PathSegment() {
     constructor(
@@ -16,5 +18,10 @@ class Line(var p1: Vector2D, var p2: Vector2D): PathSegment() {
     override fun closestT(point: Vector2D): Double{
         val l1 = mag(point - p1)
         val alpha = atan2(point.y - p1.y, point.x - p1.x)-atan2(p2.y-p1.y, p2.x-p1.x)
+	val beta = PI/2 - alpha
+	val l2 = sin(beta) * l1 / sin(PI/2)
+	val d = mag(p2 - p1)
+
+	return l2/d
     }
 }
