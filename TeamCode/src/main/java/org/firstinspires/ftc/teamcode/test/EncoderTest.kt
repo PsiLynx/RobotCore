a6bc9ade522387185b2a1d2cc344663fa85c0cb6
AS1624 <thebest1dudes@gmail.com>
2024-04-22 20:59:21 -0800

    add in unit testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/EncoderTest.kt
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.test
+
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class EncoderTest {
+
+    @Test
+    fun getAngle() {
+    }
+
+    @Test
+    fun setAngle() {
+    }
+
+    @Test
+    fun getDistance() {
+    }
+
+    @Test
+    fun setDistance() {
+    }
+
+    @Test
+    fun getDelta() {
+    }
+
+    @Test
+    fun update() {
+    }
+}
