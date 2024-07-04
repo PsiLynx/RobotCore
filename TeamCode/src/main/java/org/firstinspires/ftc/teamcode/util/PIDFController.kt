83d8628e5884e325ac3bff49e0c4930b1d73b6ef
AS1624 <thebest1dudes@gmail.com>
2024-07-03 19:25:18 -0800

    stub out closed loop controller

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/PIDFController.kt
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.util
+
+class PIDFController(P:Number=0, I:Number=0, D:Number = 0, S:Number=0, V:Number=0) {
+    var p = P.toDouble()
+    var i = I.toDouble()
+    var d = D.toDouble()
+    var s = S.toDouble()
+    var v = V.toDouble()
+
+
+}
