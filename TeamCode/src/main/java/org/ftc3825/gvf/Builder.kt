9038412abe88557ac8cf2d38b7af25c83e6379ba
AS1624 <thebest1dudes@gmail.com>
2025-01-01 09:29:26 -0900

    move heading type to builder file

--- TeamCode/src/main/java/org/ftc3825/gvf/Builder.kt
+++ TeamCode/src/main/java/org/ftc3825/gvf/Builder.kt
@@ -5,7 +5,7 @@ import org.ftc3825.subsystem.Drivetrain
 import org.ftc3825.util.Vector2D
 
 class Builder {
-    var pathSegments = arrayListOf<PathSegment>()
+    private var pathSegments = arrayListOf<PathSegment>()
     private var lastPoint = Vector2D()
     private var lastTangent = Vector2D()
 
@@ -50,3 +50,14 @@ class Builder {
 }
 fun path(builder: Builder.() -> Unit) = Builder().apply(builder).build()
 fun followPath(builder: Builder.() -> Unit) = FollowPathCommand(path(builder))
+
+sealed interface HeadingType {
+    data class Constant(val theta: Double): HeadingType
+    data class Linear(val theta1: Double, val theta2: Double): HeadingType
+    class Tangent: HeadingType
+    companion object{
+        fun tangent() = Tangent()
+        fun constant(theta: Double) = Constant(theta)
+        fun linear(theta1: Double, theta2: Double) = Linear(theta1, theta2)
+    }
+}
