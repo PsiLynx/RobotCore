04d0c04ad2a65f4b6c91e33981ec7709f162030c
AS1624 <thebest1dudes@gmail.com>
2025-01-04 10:23:23 -0900

    make servos work

--- TeamCode/src/main/java/org/ftc3825/opmodes/Servos.kt
+++ TeamCode/src/main/java/org/ftc3825/opmodes/Servos.kt
@@ -3,21 +3,34 @@ package org.ftc3825.opmodes
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
-import com.qualcomm.robotcore.hardware.Servo
+import org.ftc3825.command.internal.GlobalHardwareMap
+import org.ftc3825.component.Servo
+import org.ftc3825.component.Servo.Range.goBilda
 
 @TeleOp(name = "servos", group = "a")
 class Servos: LinearOpMode() {
 
     override fun runOpMode(){
-        val roll = hardwareMap.get(Servo::class.java, "roll")
+        GlobalHardwareMap.hardwareMap = hardwareMap
+        val test = Servo("test", goBilda)
 
         waitForStart()
 
-        roll.position = 0.0
+        test.position = 0.5
 
         sleep(1000)
 
-        roll.position = 0.5
+        test.position = 0.4
+
+        sleep(1000)
+
+        test.position = 0.2
+
+        sleep(1000)
+
+        test.position = 0.0
+
+        sleep((1e9).toLong())
 
     }
 
