c5ee329f37be6cc09a400acb849cef16e19abf0b
Mike <mandoboy@protonmail.com>
2025-11-23 00:44:39 -0900

    Fixed sim unit conversion missing.

--- TeamCode/src/test/java/test/TestShooter.kt
+++ TeamCode/src/test/java/test/TestShooter.kt
@@ -191,7 +191,7 @@ class TestShooter: TestClass() {
             log("goalArchBlocking") value goalArchBlocking
 
             if( ((t * 20) % 1) < 0.05 ) pose_hist.add(artifact.pos)
-            Logger.recordOutput("pose_hist", pose_hist.toTypedArray())
+            Logger.recordOutput("pose_hist", pose_hist.map { it / 39.37 }.toTypedArray())
             Flywheel.update(dt)
             Hood.update(dt)
 
